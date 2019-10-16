0bba3058b0849573d116342df644ea59d4326f6b
Tim Verbelen <tim.verbelen@intec.ugent.be>
2019-10-16 16:37:07 +0200

    create greeter service

---
+++ be.imec.resolution.example.api/src/be/imec/resolution/example/api/Greeter.java
@@ -0,0 +1,7 @@
+package be.imec.resolution.example.api;
+
+public interface Greeter {
+
+	public String greet(String name);
+
+}
--- be.imec.resolution.example.greeter/src/be/imec/resolution/example/greeter/GreeterImpl.java
+++ be.imec.resolution.example.greeter/src/be/imec/resolution/example/greeter/GreeterImpl.java
@@ -1,9 +1,20 @@
 package be.imec.resolution.example.greeter;
 
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import be.imec.resolution.example.api.Greeter;
 import be.imec.resolution.example.util.Util;
 
-public class GreeterImpl {
+@Component
+public class GreeterImpl implements Greeter {
 
+	@Activate
+	void activate() {
+		System.out.println("Greeter implementation activated!");
+	}
+	
+	@Override
 	public String greet(String name) {
 		return Util.cat("Hello,",name,"!");
 	}
